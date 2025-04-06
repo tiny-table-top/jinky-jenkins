3e1ce0c3fb8f1ba3d8af342390db790b56b2de62
Anthony <a.half915@mybvc.ca>
2025-04-05 18:29:52 -0600

    Showing my son

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <h1>This is my Jenkins App and now CICD</h1>
+    <div>
+      <h1>This is my Jenkins App and now CICD and a Hello to Jayden</h1>
+
+      <p>This is what I have been working on today. A React app that is on Github which is then built through Docker and Jenkins.
+      And finally deployed with Netlify. I'm sure there is more that could be done, however I think the school just wanted us to know the basics.
+      I love you my son!</p>
+    </div>
   );
 }
       
